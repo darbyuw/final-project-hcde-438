93501ec4a4048b5a838c1c946bcdd4268d95ef36
Darby Moore <darbypm@uw.edu>
2025-12-01 16:40:07 -0800

    add fishcount to gameover page

--- final-proj/src/pages/Game Over/GameOver.jsx
+++ final-proj/src/pages/Game Over/GameOver.jsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
 import "./GameOver.css";
+import { useContext } from "react";
+import { FishCountContext } from '../../context/FishCountContext.jsx';
 
 
 const Gameover = () => {
+    const { fishCount } = useContext(FishCountContext);
 
-    // reset game progress in firestore!!!
-    
-    
     return (
         <div className="gameover-container">
             <div className="gameover-content">
-                <p>Game over!</p>
+                <h1>Game over!</h1>
+                <p>You successfully collected {fishCount} fish</p>
                 <Link to="/questions" className="cta-button primary">
                     Restart
                 </Link>
